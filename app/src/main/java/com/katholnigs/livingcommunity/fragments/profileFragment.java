fca03570915ab3fd1597e8a090e3a4151708296a
KatholnigS <Steve.Katholnig@googlemail.com>
2017-06-28 16:49:11 +0200

    changed profile page +added firebase data

--- app/src/main/java/com/katholnigs/livingcommunity/fragments/profileFragment.java
+++ app/src/main/java/com/katholnigs/livingcommunity/fragments/profileFragment.java
@@ -1,22 +1,36 @@
 package com.katholnigs.livingcommunity.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.katholnigs.livingcommunity.LoginActivity;
 import com.katholnigs.livingcommunity.R;
 
-public class profileFragment extends Fragment {
+public class profileFragment extends Fragment implements View.OnClickListener {
     public static profileFragment newInstance() {
         profileFragment fragment = new profileFragment();
         return fragment;
     }
 
+    //firebase auth object
+    private FirebaseAuth firebaseAuth;
+
+    //view objects
+    private TextView textViewUserEmail;
+    private Button buttonLogout;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
     }
 
     @Override
@@ -24,4 +38,48 @@ public class profileFragment extends Fragment {
                              Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_profile, container, false);
     }
+
+    @Override
+    public void onActivityCreated (Bundle savedInstanceState){
+        super.onActivityCreated(savedInstanceState);
+
+        //initializing firebase authentication object
+        firebaseAuth = FirebaseAuth.getInstance();
+
+        //if the user is not logged in
+        //that means current user will return null
+        if(firebaseAuth.getCurrentUser() == null){
+            //closing this activity
+            getActivity().finish();
+            //starting login activity
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+        }
+
+        //getting current user
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        //initializing views
+        textViewUserEmail = (TextView) getView().findViewById(R.id.textViewUserEmail);
+        buttonLogout = (Button) getView().findViewById(R.id.buttonLogout);
+
+        //displaying logged in user name
+        textViewUserEmail.setText("Welcome "+user.getEmail());
+
+        //adding listener to button
+        buttonLogout.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        //if logout is pressed
+        if (view == buttonLogout) {
+            //logging out the user
+            firebaseAuth.signOut();
+            //closing activity
+            getActivity().finish();
+            //starting login activity
+            startActivity(new Intent(getActivity(), LoginActivity.class));
+        }
+    }
+
 }
