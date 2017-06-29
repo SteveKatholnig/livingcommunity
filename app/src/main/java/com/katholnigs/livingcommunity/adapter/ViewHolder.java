3bde1dd1cdb1539cde0338ec7b7be1a440bc0cd5
Steve <Steve.Katholnig@googlemail.com>
2017-06-29 09:49:20 +0200

    .

---
+++ app/src/main/java/com/katholnigs/livingcommunity/adapter/ViewHolder.java
@@ -0,0 +1,25 @@
+package com.katholnigs.livingcommunity.adapter;
+
+import android.support.v7.widget.CardView;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.katholnigs.livingcommunity.R;
+
+public class ViewHolder extends RecyclerView.ViewHolder {
+
+    CardView cv;
+    TextView title;
+    TextView description;
+    ImageView imageView;
+
+    public ViewHolder(View itemView) {
+        super(itemView);
+        cv = (CardView) itemView.findViewById(R.id.cardView);
+        title = (TextView) itemView.findViewById(R.id.title);
+        description = (TextView) itemView.findViewById(R.id.description);
+        imageView = (ImageView) itemView.findViewById(R.id.imageView);
+    }
+}
---
+++ app/src/main/java/com/katholnigs/livingcommunity/model/ShoppingItem.java
@@ -0,0 +1,13 @@
+package com.katholnigs.livingcommunity.model;
+
+
+public class ShoppingItem {
+    public String description;
+    public  String createdBy;
+
+    public ShoppingItem(String description, String createdBy){
+        this.description = description;
+        this.createdBy = createdBy;
+    }
+
+}
